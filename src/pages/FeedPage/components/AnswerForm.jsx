f804a36c1404be3912819fc4dc0d0bf93dd72c0c
Apologle <otl5994@naver.com>
2025-06-09 20:22:34 +0900

    폼 - 프로필 사진/닉네임 제거한 입력폼만 컴포넌트화 함 (AnswerForm.jsx)

--- src/pages/FeedPage/components/AnswerForm.jsx
+++ src/pages/FeedPage/components/AnswerForm.jsx
@@ -1,16 +1,28 @@
 import styled from 'styled-components';
-
 import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import { ButtonBrown40 } from '../../../components/Button.jsx';
 
-function AnswerForm(editMode, onEditChange, rejected, children, subjectInfo) {
+import { relativeTimeCalculator } from '../../../functions/relativeTimeCalculator.js';
+
+function AnswerForm({
+  subjectInfo,
+  children,
+  questionID,
+  editMode,
+  onEditChange,
+  rejected,
+}) {
   //editMode = 상위 컴포넌트에서 내려받는 editMode 여부 (true/false) 수정모드로 진입할지 여부를 결정.
   //onEditChange = 상위 컴포넌트에서 제어 할 목적으로 만든 set
-
+  //const time = relativeTimeCalculator(children.createdAt);
+  console.log(`children : ${children} rejected : ${rejected}`);
   const [completeState, SetCompleteState] = useState(children);
   const [answerText, setAnswerText] = useState('');
   const [text, setText] = useState('');
+  let textArray;
 
   useEffect(() => {
     if (editMode) {
@@ -31,54 +43,49 @@ function AnswerForm(editMode, onEditChange, rejected, children, subjectInfo) {
     }
   }, [editMode]);
 
-  const buttonEnabled = answerText.trim().length > 0;
+  if (
+    completeState == null ||
+    (children.content == '(empty)' && rejected == false)
+  ) {
+    const buttonEnabled = answerText.trim().length > 0; // trim 을 통해 앞 뒤 공백 제거 후 길이가 0보다 크면 true .
 
-  const handleAnswerChange = (e) => {
-    setAnswerText(e.target.value); //입력값을 answerText 에 담는다.
-    console.log(e.target.value); // 입력 확인
-  };
+    const handleAnswerChange = (e) => {
+      setAnswerText(e.target.value); //입력값을 answerText 에 담는다.
+      console.log(e.target.value); // 입력 확인
+    };
 
-  //작성 버튼 누르면 아래 실행.
-  const handleSubmitAnswer = async () => {
-    if (answerText.trim().length > 0) {
-      const requestBody = {
-        content: answerText,
-        isRejected: false,
-      };
-      if (completeState == null) {
-        try {
-          const response = await axios.post(
-            `https://openmind-api.vercel.app/16-6/questions/${questionID}/answers/`,
-            requestBody,
-          );
-        } catch (err) {
-          console.error(err);
+    const handleSubmitAnswer = async () => {
+      if (answerText.trim().length > 0) {
+        const requestBody = {
+          content: answerText,
+          isRejected: false,
+        };
+        if (completeState == null) {
+          try {
+            const response = await axios.post(
+              `https://openmind-api.vercel.app/16-6/questions/${questionID}/answers/`,
+              requestBody,
+            );
+          } catch (err) {
+            console.error(err);
+          }
+        } else {
+          try {
+            const response = await axios.put(
+              `https://openmind-api.vercel.app/16-6/answers/${children.id}/`,
+              requestBody,
+            );
+          } catch (err) {
+            console.error(err);
+          }
         }
-      } else {
-        try {
-          const response = await axios.put(
-            `https://openmind-api.vercel.app/16-6/answers/${children.id}/`,
-            requestBody,
-          );
-        } catch (err) {
-          console.error(err);
-        }
+        setAnswerText(''); // 제출 후 입력 필드 초기화
       }
-      setAnswerText(''); // 제출 후 입력 필드 초기화
-    }
-  };
+    };
 
-  return (
-    <AnswerBoxWrapper>
-      <AnswerBoxSubjectImage
-        src={subjectInfo.imageSource}
-        alt='답변자 프로필 사진'
-      />
-      <div>
+    return (
+      <AnswerBoxWrapper>
         <AnswerInput>
-          <AnswerBoxUpperlineWrapper>
-            <AnswerBoxSubjectname>{subjectInfo.name}</AnswerBoxSubjectname>
-          </AnswerBoxUpperlineWrapper>
           <SubstantialInput
             onChange={handleAnswerChange}
             value={answerText}
@@ -91,6 +98,72 @@ function AnswerForm(editMode, onEditChange, rejected, children, subjectInfo) {
             답변 완료
           </AnswerCompleteButton>
         </AnswerInput>
+      </AnswerBoxWrapper>
+    );
+  }
+
+  if (editMode) {
+    const buttonEnabled = text !== answerText;
+
+    const handleAnswerChange = (e) => {
+      setAnswerText(e.target.value); //입력값을 answerText 에 담는다.
+      console.log(e.target.value); // 입력 확인
+    };
+
+    const handleSubmitEdit = async () => {
+      if (answerText.trim().length > 0) {
+        const requestBody = {
+          content: answerText,
+          isRejected: false,
+        };
+        try {
+          const response = await axios.put(
+            `https://openmind-api.vercel.app/16-6/answers/${children.id}/`,
+            requestBody,
+          );
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      setAnswerText(''); // 제출 후 입력 필드 초기화
+      onEditChange(false);
+    };
+
+    return (
+      <AnswerBoxWrapper>
+        <AnswerInput>
+          <SubstantialInput
+            onChange={handleAnswerChange}
+            value={answerText}
+          ></SubstantialInput>
+          <AnswerCompleteButton
+            onClick={handleSubmitEdit}
+            disabled={!buttonEnabled}
+          >
+            수정 완료
+          </AnswerCompleteButton>
+        </AnswerInput>
+      </AnswerBoxWrapper>
+    );
+  }
+
+  return (
+    <AnswerBoxWrapper>
+      <div>
+        <AnswerBoxUpperlineWrapper>
+          <AnswerBoxCreatedAt>
+            {completeState ? (
+              relativeTimeCalculator(children.createdAt)
+            ) : (
+              <div></div>
+            )}
+          </AnswerBoxCreatedAt>
+        </AnswerBoxUpperlineWrapper>
+        {rejected ? (
+          <AnswerBoxText isRejected={rejected}>답변 거절</AnswerBoxText>
+        ) : (
+          completeState && <AnswerBoxText>{children.content}</AnswerBoxText>
+        )}
       </div>
     </AnswerBoxWrapper>
   );
@@ -98,11 +171,15 @@ function AnswerForm(editMode, onEditChange, rejected, children, subjectInfo) {
 
 export default AnswerForm;
 
-const AnswerInput = styled.div`
+const AnswerBoxWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 8px;
-  width: 100%;
+  gap: 12px;
+`;
+
+const AnswerBoxCreatedAt = styled.span`
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.gray40};
 `;
 
 const AnswerBoxUpperlineWrapper = styled.div`
@@ -112,9 +189,12 @@ const AnswerBoxUpperlineWrapper = styled.div`
   margin-bottom: 4px;
 `;
 
-const AnswerBoxSubjectname = styled.span`
-  font-size: 18px;
+const AnswerBoxText = styled.span.withConfig({
+  shouldForwardProp: (prop) => prop !== 'isRejected',
+})`
+  font-size: 16px;
   font-weight: 400;
+  color: ${(props) => props.isRejected && props.theme.color.red50};
 `;
 
 const SubstantialInput = styled.textarea`
@@ -142,14 +222,9 @@ const AnswerCompleteButton = styled(ButtonBrown40)`
   font-size: 16px;
 `;
 
-const AnswerBoxWrapper = styled.div`
+const AnswerInput = styled.div`
   display: flex;
-  gap: 12px;
-`;
-
-const AnswerBoxSubjectImage = styled.img`
-  width: 48px;
-  height: 48px;
-  object-fit: cover;
-  border-radius: 9999px;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
 `;
